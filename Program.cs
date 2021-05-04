46d0d68fe38a491c286d184adaf23751308f586f
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2021-05-04 13:05:09 -0600

    Sesión 1 - 04/05/2021
    - Tipos de datos por valor y por referencia
    - Cadenas de texto con formato

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ namespace MusicApp
 {
     class Program
     {
+        const int LARGO_DE_LINEA = 60;
         static void Main(string[] args)
         {
             var album1 = new Album
@@ -51,16 +52,51 @@ namespace MusicApp
             album1.Artista = artista1;
             album1.Critica = critica1;
 
-            Console.WriteLine("".PadLeft(60, '-'));
-            Console.WriteLine($"Album: {album1.Titulo}\nArtista: {album1.Artista.NombreArtistico}");
-            Console.WriteLine("".PadLeft(60, '-'));
-            Console.WriteLine($"Críticas\nRolling Stone: {album1.Critica.RollingStone}");
-            Console.WriteLine($"MTV: {album1.Critica.Mtv}");
-            Console.WriteLine($"Musica Maniac: {album1.Critica.MusicManiac}");
+            
+            imprimirLineaSencilla();
+            Console.WriteLine($"{"Album:",9} {album1.Titulo}\n{"Artista:",9} {album1.Artista.NombreArtistico}");
+            imprimirTitulo("Críticas");
+            Console.WriteLine($"{"Rolling Stone:",14} {album1.Critica.RollingStone}");
+            Console.WriteLine($"{"MTV:",14} {album1.Critica.Mtv}");
+            Console.WriteLine($"{"Music Maniac:",14} {album1.Critica.MusicManiac}");
 
             // TODO: imprimir en pantalla la información completa del album
             // TODO: realizar lo conducente para imprimir en pantalla la informacion del artista,
             //       y sus datos de contacto
+
+            /*
+            // variables de tipo primitivo, se referencian a la porción de memoria de cada una
+            int a = 10;
+            int b = a;
+            Console.WriteLine($"a:{a} b:{b}");
+            a = 20;
+            Console.WriteLine($"a:{a} b:{b}");
+
+            // variable de tipo referencia, se referencian a la porción de memoria compartida
+            var artistax = new Artista {
+                Id = 2,
+                NombreArtistico = "Snoop Dogg",
+                NombreReal = "Don Motorolo"
+            };
+
+            var artistax2 = artistax;
+            Console.WriteLine($"x:{artistax.NombreReal} x2:{artistax2.NombreReal}");
+            artistax.NombreReal = "Calvin Cordozar Broadus Jr.";
+            Console.WriteLine($"x:{artistax.NombreReal} x2:{artistax2.NombreReal}");
+            */
+        }
+
+        private static void imprimirTitulo(string texto)
+        {
+            var longitud = texto.Length/2;
+            imprimirLineaSencilla();
+            Console.WriteLine($"{texto,LARGO_DE_LINEA/2}");
+            imprimirLineaSencilla();
+        }
+
+        private static void imprimirLineaSencilla()
+        {
+            Console.WriteLine("".PadLeft(LARGO_DE_LINEA, '-'));
         }
     }
 }
